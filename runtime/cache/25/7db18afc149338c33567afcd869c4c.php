4ba591b78d8d5b07ed9dfdb30a4ac70a7d678620
majiameng <666@majiameng.com>
2017-11-03 17:57:25 +0800

    del cache

--- runtime/cache/25/7db18afc149338c33567afcd869c4c.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{i:1;a:5:{s:2:"id";i:1;s:4:"name";s:12:"普通用户";s:8:"discount";i:100;s:9:"min_exper";i:0;s:9:"max_exper";i:0;}i:2;a:5:{s:2:"id";i:2;s:4:"name";s:6:"会员";s:8:"discount";i:95;s:9:"min_exper";i:100;s:9:"max_exper";i:1000;}i:3;a:5:{s:2:"id";i:3;s:4:"name";s:12:"超级会员";s:8:"discount";i:90;s:9:"min_exper";i:1000;s:9:"max_exper";i:10000;}}
