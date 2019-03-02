89eb5967f374f59bf9d8384d12027968852e5296
majiameng <879042886@qq.com>
2019-03-02 10:35:37 +0800

    update admin.php

--- public/admin.php
+++ public/admin.php
@@ -17,7 +17,7 @@ header('Content-Type:text/html;charset=utf-8');
 if(version_compare(PHP_VERSION,'5.5.0','<'))  die('PHP版本过低，最少需要PHP5.5，请升级PHP版本！');
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/app/');
+define('APP_PATH', __DIR__ . '/../app/');
 
 // 定义入口为admin
 define('ENTRANCE','admin');
@@ -30,7 +30,7 @@ if(!is_file(APP_PATH.'install/install.lock')) {
 }
 
 // 加载框架引导文件
-require __DIR__ . '/thinkphp/start.php';
+require __DIR__ . '/../thinkphp/start.php';
 
 /*
 *                #####################################################
